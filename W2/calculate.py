37d698f8c7133b80559ef0a71ff99dd67af03092
hidarrazka <haidarazka.0807@gmail.com>
2026-02-24 13:49:08 +0700

    update fitur

--- W2/calculate.py
+++ W2/calculate.py
@@ -1,18 +1,22 @@
 def calculate_mean(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    mean = total/len(numbers)
     # Calculate the mean of a list of numbers
-    return;
+    return mean
     
 def calculate_median(numbers):
     # Calculate the median of a list of numbers
-    return;
+    return
     
 def calculate_maximum(numbers):
     # Calculate the maximum of a list of numbers
-    return;
+    return
     
 def calculate_minimum(numbers):
     # Calculate the minimum of a list of numbers
-    return;
+    return
     
 def calculate_all(numbers):
     # Calculate all statistics (mean, median, maximum, minimum) and return as a dictionary
